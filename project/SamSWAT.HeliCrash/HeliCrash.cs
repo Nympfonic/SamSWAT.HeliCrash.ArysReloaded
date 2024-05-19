21449a6ed49111cfdc5a601d9cde4778a39d56be
Nympfonic <arys.steam@gmail.com>
2024-05-20 00:22:02 +0100

    Added NavMeshObstacle component to helicrash object

--- project/SamSWAT.HeliCrash/HeliCrash.cs
+++ project/SamSWAT.HeliCrash/HeliCrash.cs
@@ -3,6 +3,7 @@ using Aki.Custom.Airdrops.Utils;
 using Comfort.Common;
 using EFT;
 using EFT.Interactive;
+using EFT.InventoryLogic;
 #if DEBUG
 using EFT.UI;
 #endif
@@ -10,6 +11,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace SamSWAT.HeliCrash.ArysReloaded
 {
@@ -20,16 +22,22 @@ namespace SamSWAT.HeliCrash.ArysReloaded
 
         public async void Init(string location)
         {
-            var heliLocation = GetHeliCrashLocation(location);
+            Location heliLocation = GetHeliCrashLocation(location);
 #if DEBUG
             var error = $"Heli crash site spawned at position x: {heliLocation.Position.x}, y: {heliLocation.Position.y}, z: {heliLocation.Position.z}";
             ConsoleScreen.LogWarning(error);
             Plugin.LogSource.LogWarning(error);
 #endif
-            var choppa = Instantiate(await LoadChoppaAsync(), heliLocation.Position, Quaternion.Euler(heliLocation.Rotation));
-            var container = choppa.GetComponentInChildren<LootableContainer>();
+            GameObject choppa = Instantiate(await LoadChoppaAsync(), heliLocation.Position, Quaternion.Euler(heliLocation.Rotation));
+            
+            var navMeshObstacle = choppa.AddComponent<NavMeshObstacle>();
+            var collider = choppa.GetComponentInChildren<Collider>();
+            navMeshObstacle.carving = true;
+            navMeshObstacle.center = collider.bounds.center;
+            navMeshObstacle.size = collider.bounds.size;
 
-            var itemFactory = Singleton<ItemFactory>.Instance;
+            LootableContainer container = choppa.GetComponentInChildren<LootableContainer>();
+            ItemFactory itemFactory = Singleton<ItemFactory>.Instance;
 
             if (!itemFactory.ItemTemplates.TryGetValue(CRATE_ID, out var itemTemplate))
             {
@@ -38,7 +46,7 @@ namespace SamSWAT.HeliCrash.ArysReloaded
 
             var itemFactoryUtil = new ItemFactoryUtil();
             AirdropLootResultModel lootResult = itemFactoryUtil.GetLoot();
-            var itemCrate = itemFactory.CreateItem(CRATE_ID, itemTemplate._id, null);
+            Item itemCrate = itemFactory.CreateItem(CRATE_ID, itemTemplate._id, null);
             LootItem.CreateLootContainer(container, itemCrate, "Heavy crate", Singleton<GameWorld>.Instance);
             itemFactoryUtil.AddLoot(container, lootResult);
         }
